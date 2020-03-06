13534f0b0b2695e170db5cd030e7daaae8b356ed
srividya9922 <60392276+srividya9922@users.noreply.github.com>
2020-03-06 14:05:02 +0530

    Add files via upload
    no

---
+++ Calculator.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+class Addition
+{
+	public void operation(double num1,double num2)
+	{
+	System.out.println((num1+num2));
+	}
+}
+
+class Subtraction
+{
+	public void operation(double num1,double num2)
+	{
+	System.out.println((num1-num2));
+	}
+}
+
+
+class Multiplication
+{
+	public void operation(double num1,double num2)
+	{
+	System.out.println((num1*num2));
+	}
+}
+
+class Division
+{
+	public void operation(double num1,double num2)
+	{
+	System.out.println((num1/num2));
+	}
+}
+
+class Calculator
+{
+	public static void main(String arg[])
+	{
+
+	Addition a=new Addition();
+	Subtraction s=new Subtraction();
+	Multiplication m=new Multiplication();
+	Division d=new Division();
+	System.out.println("########       MENU      ########");
+	
+	System.out.println("1: ADDITION\n2: SUBTRACTION\n3: MULTIPLICATION\n4: DIVISION");
+	
+	Scanner sc=new Scanner(System.in);
+	int ch;
+	do
+	{
+	System.out.println("Enter any two numbers:");
+	double number1=sc.nextDouble();
+	double number2=sc.nextDouble();
+
+	System.out.println("Choose your operations:");
+	int choice=sc.nextInt();
+	switch(choice)
+	{
+	case 1:
+		a.operation(number1,number2);
+		break;
+	case 2:
+		s.operation(number1,number2);
+		break;
+	case 3:
+		m.operation(number1,number2);
+		break;
+	case 4:
+		d.operation(number1,number2);
+		break;
+	}
+	}while(ch!=1);
+	}
+}
+	
+			
